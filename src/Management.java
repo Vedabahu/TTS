1a620cb1316fa30739bac54ba1cb458362cb6c86
Vedabahu <79869936+Vedabahu@users.noreply.github.com>
2024-10-18 19:01:25 +0530

    formatted everything

--- src/Management.java
+++ src/Management.java
@@ -1,23 +1,16 @@
 
 public class Management {
-	private int id;
-	private String name;
+    private int id;
+    private String name;
 
-	Management(int id, String name) {
-		this.id = id;
-		this.name = name;
-	}
+    Management(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
-	int get_id() {
-		return id;
-	}
+    int get_id() { return id; }
 
-	String get_name() {
-		return name;
-	}
+    String get_name() { return name; }
 
-	void set_name(String name) {
-		this.name = name;
-	}
-
+    void set_name(String name) { this.name = name; }
 }
--- src/Room.java
+++ src/Room.java
@@ -1,23 +1,16 @@
 
 public class Room {
-	private int room_number;
-	private boolean is_occupied = false;
+    private int room_number;
+    private boolean is_occupied = false;
 
-	Room(int room_number, boolean is_occupied) {
-		this.room_number = room_number;
-		this.is_occupied = is_occupied;
-	}
+    Room(int room_number, boolean is_occupied) {
+        this.room_number = room_number;
+        this.is_occupied = is_occupied;
+    }
 
-	int get_room_number() {
-		return room_number;
-	}
+    int get_room_number() { return room_number; }
 
-	boolean isOccupied() {
-		return is_occupied;
-	}
+    boolean isOccupied() { return is_occupied; }
 
-	void set_is_occupied(boolean occupied) {
-		is_occupied = occupied;
-	}
-
+    void set_is_occupied(boolean occupied) { is_occupied = occupied; }
 }
