c82aca9a4daf6f109990f69f4363e913f0b8b705
Vedabahu <79869936+Vedabahu@users.noreply.github.com>
2024-10-31 15:43:59 +0530

    added ability to save as image

--- src/oop/veda/TimeTable.java
+++ src/oop/veda/TimeTable.java
@@ -68,103 +68,100 @@ public class TimeTable {
                     data[i][j + k] = ""; // Keep it empty
             }
         }
-        
+
         String[] coursesHeadings = {"Course Title", "Course Code", "L-T-P-S-C", "Pre-requsite",
-              "Lecturer", "Lab Assistance"};
-        String[][] coursesArray = getCoursesAsArray();
-        
+                "Lecturer", "Lab Assistance"};
+        String[][] coursesArray  = getCoursesAsArray();
+
         String s1 = printTableHelper(data, headings);
         String s2 = printTableHelper(coursesArray, coursesHeadings, 200);
-        
-        writeToFile("timeTable/CSE-B.png", s1 + s2);
-        
 
+        writeToFile("timeTable/CSE-B.png", s1 + s2);
     }
 
     private String printTableHelper(String[][] data, String[] headings) {
-    	return printTableHelper(data, headings, 250);
+        return printTableHelper(data, headings, 250);
     }
-    
-	private String printTableHelper(String[][] data, String[] headings, int width) {
-		AsciiTable act = new AsciiTable();
-		
-		act.getContext().setWidth(width);
-		
-		act.setPadding(10);
-		act.addRule();
-		act.addRow(Arrays.asList(headings)); // no repetition
-		act.addRule();
-		
-		ArrayList<ArrayList<String>> data2d = new ArrayList<ArrayList<String>>();
-		
-		for (int i = 0 ; i < data.length; i++) {
-			data2d.add(new ArrayList<String>());
-			for (int j = 0; j < data[i].length; j++) {
-				data2d.get(i).add(data[i][j]);
-				if (j > 1) {
-					if (data2d.get(i).get(j).equals(data2d.get(i).get(j-1))) {
-						data2d.get(i).set(j-1, null);
-					}
-				}
-			}
-		}
-		
-		for (int i = 0; i < data2d.size(); i++) {
-			act.addRow(data2d.get(i));
-			act.addRule();
-		}
-		
-		act.setTextAlignment(TextAlignment.CENTER);
-		act.getContext().setFrameLeftRightMargin(5);
-		act.getContext().setFrameTopBottomMargin(1);
-		System.out.println(act.render());
-		
-		return act.render();
-	}
-	
-	private void writeToFile(String filepath, String contents) {
-		Font outputFont = new Font(Font.MONOSPACED, Font.PLAIN, 28);		
-		FontMetrics fm = (new Canvas()).getFontMetrics(outputFont);
-		
-		String[] lines = contents.split("\n");
-		
-		final int WIDTH = fm.stringWidth(lines[0]); // First line width is always the maximum
-		final int HEIGHT = fm.getHeight() * lines.length + 1;
 
-		
-		BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
-		Graphics2D g2d = image.createGraphics(); 
-		
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        
+    private String printTableHelper(String[][] data, String[] headings, int width) {
+        AsciiTable act = new AsciiTable();
+
+        act.getContext().setWidth(width);
+
+        act.setPadding(10);
+        act.addRule();
+        act.addRow(Arrays.asList(headings)); // no repetition
+        act.addRule();
+
+        ArrayList<ArrayList<String>> data2d = new ArrayList<ArrayList<String>>();
+
+        for (int i = 0; i < data.length; i++) {
+            data2d.add(new ArrayList<String>());
+            for (int j = 0; j < data[i].length; j++) {
+                data2d.get(i).add(data[i][j]);
+                if (j > 1) {
+                    if (data2d.get(i).get(j).equals(data2d.get(i).get(j - 1))) {
+                        data2d.get(i).set(j - 1, null);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < data2d.size(); i++) {
+            act.addRow(data2d.get(i));
+            act.addRule();
+        }
+
+        act.setTextAlignment(TextAlignment.CENTER);
+        act.getContext().setFrameLeftRightMargin(5);
+        act.getContext().setFrameTopBottomMargin(1);
+        System.out.println(act.render());
+
+        return act.render();
+    }
+
+    private void writeToFile(String filepath, String contents) {
+        Font outputFont = new Font(Font.MONOSPACED, Font.PLAIN, 28);
+        FontMetrics fm  = (new Canvas()).getFontMetrics(outputFont);
+
+        String[] lines = contents.split("\n");
+
+        final int WIDTH  = fm.stringWidth(lines[0]); // First line width is always the maximum
+        final int HEIGHT = fm.getHeight() * lines.length + 1;
+
+        BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d      = image.createGraphics();
+
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(
+                RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
         g2d.setColor(Color.BLACK);
-//        g2d.setColor(Color.WHITE);
+        //        g2d.setColor(Color.WHITE);
         g2d.fillRect(0, 0, WIDTH, HEIGHT);
-        
+
         g2d.setColor(Color.LIGHT_GRAY);
-//        g2d.setColor(Color.BLACK);
+        //        g2d.setColor(Color.BLACK);
         g2d.setFont(outputFont);
-        
+
         FontMetrics metrics = g2d.getFontMetrics(g2d.getFont());
-        int lineHeight = metrics.getHeight();
-        int y = metrics.getAscent();
-        
+        int lineHeight      = metrics.getHeight();
+        int y               = metrics.getAscent();
+
         for (String line : lines) {
-        	g2d.drawString(line, 10, y);
-        	y += lineHeight;
+            g2d.drawString(line, 10, y);
+            y += lineHeight;
         }
         g2d.dispose();
-
 
         try {
             ImageIO.write(image, "png", new File(filepath));
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-	}
-	
-	public void makeTheTimeTable() {
+    }
+
+    public void makeTheTimeTable() {
         /*
          * Timings: 9:00 to 9:30 | 9:30 10 | 10 to 10:30 | <break> | 10:45 to
          * 11:15 | 11:15 to 11 : 45 | 11:45 to 12:15 | 12:15 to 12:45 | 12:45 to
