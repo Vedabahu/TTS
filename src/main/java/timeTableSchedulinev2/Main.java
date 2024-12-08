34d995dabe0feaa2659369e108614f4cafb63413
Vedabahu <79869936+Vedabahu@users.noreply.github.com>
2024-12-08 21:28:45 +0530

    did it
    - The 2nd sem CSE LTPSC is not given. I have used dummy LTPSC
    - ECE is not give, previous one has been used

--- src/main/java/timeTableSchedulinev2/Main.java
+++ src/main/java/timeTableSchedulinev2/Main.java
@@ -9,9 +9,9 @@ public class Main {
 
         TimeTable tt = new TimeTable(s);
         tt.makeTimeTable();
-        tt.printTimeTable(0, "CSE-1st-sem.png");
-        tt.printTimeTable(1, "CSE-3rd-sem.png");
-        tt.printTimeTable(2, "CSE-5th-sem.png");
+        tt.printTimeTable(0, "CSE-2nd-sem.png");
+        tt.printTimeTable(1, "CSE-4th-sem.png");
+        tt.printTimeTable(2, "CSE-6th-sem.png");
 
         ArrayList<Course> c1   = ReadCSE.readCoursesFromCSV("inputFiles/ECE.csv");
         ArrayList<Semester> s1 = Semester.convertToSems(c1);
@@ -27,8 +27,8 @@ public class Main {
 
         TimeTable tt2 = new TimeTable(s2);
         tt2.makeTimeTable();
-        tt2.printTimeTable(0, "DSAI-1st-sem.png");
-        tt2.printTimeTable(1, "DSAI-3st-sem.png");
-        tt2.printTimeTable(2, "DSAI-5st-sem.png");
+        tt2.printTimeTable(0, "DSAI-2nd-sem.png");
+        tt2.printTimeTable(1, "DSAI-4th-sem.png");
+        tt2.printTimeTable(2, "DSAI-6th-sem.png");
     }
 }
